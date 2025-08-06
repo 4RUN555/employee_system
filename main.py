b6dc630e77e0f2b4f414abaaff8b57d8857e5929
4RUN555 <68030316@kmitl.ac.th>
2025-08-06 16:24:58 +0700

    x

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 from add_employee import add_employee 
 from remove_employee import remove_employee
-from search_employee import search_employee 
+from search_employee import search_employee
+
+employees = [] 
+employees = add_employee(employees, "Alice")
+employees = add_employee(employees, "Bob")
+employees = remove_employee(employees, "Alice")
